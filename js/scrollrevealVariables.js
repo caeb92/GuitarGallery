0121ebe1c530091a07b4df16ef890605f021a3dc
caeb92 <30680798+caeb92@users.noreply.github.com>
2017-09-08 13:39:50 -0300

    Primer commit
    Practica bootstrap 4

---
+++ js/scrollrevealVariables.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+sr.reveal('.items-navbar', {
+  duration: 2000,
+  origin:'left',
+  distance:'200px'
+});
+sr.reveal('.items-izq', {
+  duration: 1500,
+  origin:'left',
+  distance:'300px'
+});
+sr.reveal('.items-der', {
+  duration: 1500,
+  origin:'right',
+  distance:'300px'
+});
+sr.reveal('.items-btn', {
+  duration: 2000,
+  delay: 2000,
+  origin:'bottom'
+});
